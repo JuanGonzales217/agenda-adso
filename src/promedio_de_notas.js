943dbdfbbd65d6b8814bbe348e96ae1e421d7cf8
Juan <juanhoraciogonzalesarboledfamail.com>
2025-10-23 13:33:27 -0500

    Agrego ejercicio de promedio de notas en JavaScript

---
+++ src/promedio_de_notas.js
@@ -0,0 +1,8 @@
+const nombre = "juan Horacio Gonzales arboleda";
+const notas = [4.0,3.5,2.8]
+
+const promedio =(notas[0]+ notas[1] + notas[2])/2
+
+console.log(`Aprendiz: ${nombre}`);
+console.log(`Promedio: ${promedio}`);
+console.log(`Estado: ${promedio >=3 ? "Aprobo" : "No aprobo"}`)
